1b88a6065446ce92145fc292d51e0b98716fed41
Drew Gordon <andrewbengordon@gmail.com>
2022-11-11 17:48:05 -0700

    Wrote Tests

---
+++ TheMovieWatchlist.Tests/WatchlistTest.cs
@@ -0,0 +1,25 @@
+﻿using TheMovieWatchlist.Components;
+using TheMovieWatchlist.Data;
+
+namespace TheMovieWatchlist.Tests;
+
+public class WatchlistTest : BunitTestContext
+{
+    [Test]
+    public void WatchlistMovieTest()
+    {
+        var component = RenderComponent<ActorRoleCard>(parameter =>
+            parameter.Add(p =>
+                p.Actor, new Actorlist
+                {
+                    id = "1",
+                    name = "Drew",
+                    image = "file.jpg",
+                    asCharacter = "Karl"
+                }
+                ));
+
+        Assert.AreEqual(component.FindAll("p")[0].InnerHtml, "Drew");
+        Assert.AreEqual(component.FindAll("p")[1].InnerHtml, "Karl");
+    }
+}
